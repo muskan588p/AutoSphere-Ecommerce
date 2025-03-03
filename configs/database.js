8282ffabdd26ff859e7f01554849b6fe88c0fd77
Piyush Sacher <piyushsacher18@gmail.com>
2025-03-03 12:08:20 +0530

    General database structure

--- configs/database.js
+++ configs/database.js
@@ -1,1 +1,12 @@
-//database
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('EComDB', 'root', 'admin@1234', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+sequelize.authenticate()
+    .then(() => console.log('Database connected...'))
+    .catch(err => console.log('Error: ' + err));
+
+module.exports = sequelize;
