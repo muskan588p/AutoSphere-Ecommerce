6c45dcdab953628978b38710c21bb9a0a0c65a00
muskan588p <musk2806@gmail.com>
2025-03-05 23:22:57 +0530

    database connected

--- backend/configs/database.js
+++ backend/configs/database.js
@@ -1,12 +1,28 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
+// require("dotenv").config("../.env"); 
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
-const sequelize = new Sequelize('EComDB', 'root', 'admin@1234', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST, 
+    dialect: "mysql", 
+    logging: false, 
+    port: process.env.DB_PORT,
+  }
+);
 
-sequelize.authenticate()
-    .then(() => console.log('Database connected...'))
-    .catch(err => console.log('Error: ' + err));
+// Test Database Connection
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection established successfully.");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+})();
 
-module.exports = sequelize;
+module.exports = { sequelize };
